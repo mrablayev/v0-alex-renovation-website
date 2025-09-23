707584529cb6e14478295e2eb539ebaaab7436e2
mrablayev <59255869+mrablayev@users.noreply.github.com>
2025-09-23 00:51:23 -0400

    Remove React import from Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import React from "react"
 import { Navigation } from "@/components/navigation"
 import { Footer } from "@/components/footer"
 import { Button } from "@/components/ui/button"
