d88e2c89b148673dfa8f57aab8aaeb7ac04342a8
mrablayev <59255869+mrablayev@users.noreply.github.com>
2025-09-23 00:54:40 -0400

    Remove duplicate 'use client' and React import

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 "use client"
 
-"use client"
-
-import React from "react"
 import { Navigation } from "@/components/navigation"
 import { Footer } from "@/components/footer"
 import { Button } from "@/components/ui/button"
